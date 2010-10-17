7da0c12b4cc2264b81f6f337e0dd2cd74a8339ce
Dr Nic Williams <drnicwilliams@gmail.com>
2010-10-17 14:02:36 -0700

    added cucumber features

---
+++ features/support/env.rb
@@ -0,0 +1,14 @@
+require File.dirname(__FILE__) + "/../../lib/myyard"
+
+require 'bundler'
+gem 'cucumber'
+gem 'rspec'
+
+Before do
+  @tmp_root = File.dirname(__FILE__) + "/../../tmp"
+  @home_path = File.expand_path(File.join(@tmp_root, "home"))
+  @lib_path  = File.expand_path(File.dirname(__FILE__) + "/../../lib")
+  FileUtils.rm_rf   @tmp_root
+  FileUtils.mkdir_p @home_path
+  ENV['HOME'] = @home_path
+end
