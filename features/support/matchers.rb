a2e4118f5592b349c0807cc79504f9f2c9db2e49
Dr Nic Williams <drnicwilliams@gmail.com>
2010-10-17 22:10:56 -0700

    update old rspec extension to rspec 2

--- features/support/matchers.rb
+++ features/support/matchers.rb
@@ -1,9 +1,7 @@
 module Matchers
-  def contain(expected)
-    simple_matcher("contain #{expected.inspect}") do |given, matcher|
-      matcher.failure_message = "expected #{given.inspect} to contain #{expected.inspect}"
-      matcher.negative_failure_message = "expected #{given.inspect} not to contain #{expected.inspect}"
-      given.index expected
+  RSpec::Matchers.define :contain do |expected_text|
+    match do |text|
+      text.index expected_text
     end
   end
 end
